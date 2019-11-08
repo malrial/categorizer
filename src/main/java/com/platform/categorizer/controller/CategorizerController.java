3eb8219ed4a3bf29910d5e43e9a38a7c295a2e71
anxo <angel.lopezrial@gmail.com>
2019-11-08 11:11:13 +0100

    commit login

--- src/main/java/com/platform/categorizer/controller/CategorizerController.java
+++ src/main/java/com/platform/categorizer/controller/CategorizerController.java
@@ -1,16 +1,23 @@
 package com.platform.categorizer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategorizerController {
+	
+	Logger logger = LoggerFactory.getLogger(CategorizerController.class);
+    
+
 
     private static final String template = "Categories, %s!";
 
     @RequestMapping("/categories")
     public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	logger.info("Hello World");
         return String.format(template, name);
                             
     }
